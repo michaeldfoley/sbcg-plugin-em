f3315e925b651e981b9182b10973a960d345eb56
Michael Foley <michaeldfoley@gmail.com>
2015-06-03 13:41:53 -0400

    added not logged in booking conditions to event manager plugin

--- login.php
+++ login.php
@@ -14,4 +14,30 @@ function _sbcg_em_login_placeholders($replace, $EM_Event, $result){
 	}
 	return $replace;
 }
+
+add_action('em_event_output_condition', '_sbcg_em_has_bookings_not_logged_in_condition', 1, 4);
+function _sbcg_em_has_bookings_not_logged_in_condition($replacement, $condition, $match, $EM_Event){
+	if( is_object($EM_Event) && $condition === 'has_bookings_not_logged_in' ){
+  	if ( !is_user_logged_in() && $EM_Event->event_rsvp && get_option('dbem_rsvp_enabled') ) {
+  		$replacement = preg_replace("/\{\/?$condition\}/", '', $match);
+  	}else{
+  		$replacement = '';
+  	}
+	}
+	if( is_object($EM_Event) && $condition === 'fully_booked_not_logged_in' ){
+  	if ( !is_user_logged_in() && $EM_Event->event_rsvp && $EM_Event->get_bookings()->get_available_spaces() <= 0 ) {
+  		$replacement = preg_replace("/\{\/?$condition\}/", '', $match);
+  	}else{
+  		$replacement = '';
+  	}
+	}
+	if( is_object($EM_Event) && $condition === 'has_spaces_not_logged_in' ){
+  	if ( !is_user_logged_in() && $EM_Event->event_rsvp && $EM_Event->get_bookings()->get_available_spaces() > 0 ) {
+  		$replacement = preg_replace("/\{\/?$condition\}/", '', $match);
+  	}else{
+  		$replacement = '';
+  	}
+	}
+	return $replacement;
+}
 ?>
